757d8869a2532246aaf9b8b2beaed85c6ec7dfac
Aleksey Kliger <aleksey@xamarin.com>
2017-07-17 15:25:47 -0400

    Reformat some comments to make Haddock happy (NFC)

--- src/Graph.hs
+++ src/Graph.hs
@@ -13,7 +13,7 @@ import Data.Generics
 import Data.Monoid ((<>))
 import Language.C.Data.Ident (Ident(..))
 import qualified Language.C.Data.Ident
-import Language.C.Syntax.AST -- *
+import Language.C.Syntax.AST -- \*
 import Types
 import qualified Data.HashSet as HashSet
 import qualified Data.Map as Map
@@ -199,7 +199,7 @@ callMapFromNameMap = Map.fromList . map fromEntry . Map.toList
       CIf expr stat1 mStat2 _pos
         -> fromExpression expr
         <> singletonCallSequence (fromStatement stat1 `Choice` foldMap fromStatement mStat2)
-      -- | switch statement @CSwitch selectorExpr switchStmt@, where
+      -- switch statement @CSwitch selectorExpr switchStmt@, where
       -- @switchStmt@ usually includes /case/, /break/ and /default/
       -- statements
       CSwitch expr stat _pos
